36bd8945edcd82f5666cb4b9820c2ca68ea6c361
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-02 20:59:55 -0700

    better art

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::usize;
 use rand::{self, Rng};
 
 fn prime() -> usize {
-    let rand: usize = rand::thread_rng().gen_range(0..1000);
+    let rand: usize = rand::thread_rng().gen_range(2..2000);
     // sieve method for small prime gaps.. for larger sampling sizes more complex gap analysis is needed
     for x in 2..(rand / 2) {
         if rand % x == 0 {
@@ -13,23 +13,21 @@ fn prime() -> usize {
     // n2 is prime after making it through all numbers previous on [2,n2/2]
     return rand;
 }
-fn isprime( i:usize) ->bool{
+fn isprime(i: usize) -> bool {
     for x in 2..(i / 2) {
         if i % x == 0 {
             return false;
         }
     }
 
-return true;
+    return true;
 }
 fn main() {
-    println!("----F(X) LOADING----");
     println!("< WELCOME TO ART GENERATOR 3000!");
 
-    let bound = prime();
     for x in 1..prime() {
         println!("");
-        if(isprime(x)){
+        if isprime(x) {
             for z in 0..x {
                 print!("*");
                 for y in 0..x {
@@ -37,7 +35,6 @@ fn main() {
                 }
             }
         }
-
     }
 
     println!("");
